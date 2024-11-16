47bb84ca9e44345a4ae373697a0cdf7807d16c1b
aversii <lucasaversi@gmail.com>
2024-11-16 01:10:04 -0300

    Deploy cfg

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { User } from './model/user';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
   
   }
