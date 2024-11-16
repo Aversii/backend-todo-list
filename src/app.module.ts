709de0d7adc18b4092cf5649409aabd0ed3b7831
aversii <lucasaversi@gmail.com>
2024-11-16 16:50:34 -0300

    fix: cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';  // Para ler variáveis de ambiente
+import { ConfigModule } from '@nestjs/config';
 import { User } from './model/user';
-import { UserModule } from './user.module';
 import { Task } from './model/tasks';
+import { UserModule } from './user.module';
 import { TaskModule } from './task.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,  // Torna as variáveis de ambiente acessíveis globalmente
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'oracle',  // Define o tipo como 'oracle'
-      host: process.env.DB_HOST,  // Endereço do banco de dados
-      port: parseInt(process.env.DB_PORT, 10),  // Porta do banco de dados
-      username: process.env.DB_USER,  // Usuário do banco de dados
-      password: process.env.DB_PASSWORD,  // Senha do banco de dados
-      sid: process.env.DB_SID,  // SID do banco Oracle
-      entities: [User,Task],  // Defina as entidades do TypeORM
-      synchronize: true,  // Sincroniza automaticamente as entidades com o banco de dados (use com cautela em produção)
+      type: 'oracle',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 1521,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      sid: process.env.DB_SID,
+      entities: [User, Task],
+      synchronize: true,
     }),
-    UserModule, TaskModule // Registra o módulo User
+    UserModule,
+    TaskModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todas as origens (use com cautela)
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    }).forRoutes('*'); // Aplica o middleware para todas as rotas
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 import { User } from './model/user';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Permite todas as origens
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'], // Certifique-se de permitir cabeçalhos necessários  
-  });  await app.listen(process.env.PORT ?? 3000);
-  
+  const app = await NestFactory.create(AppModule, { cors: true });  
+  await app.listen(process.env.PORT ?? 3000); 
   }
 bootstrap();
